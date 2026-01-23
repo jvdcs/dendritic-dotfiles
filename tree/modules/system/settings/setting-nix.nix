e59849917f512ecef21fa9eda3062bafc5ff800d
jvdcs <javedcorreossial@gmail.com>
2026-01-23 20:10:28 +0800

    switch

--- tree/modules/system/settings/setting-nix.nix
+++ tree/modules/system/settings/setting-nix.nix
@@ -18,13 +18,13 @@
           "https://nix-community.cachix.org"
           "https://cache.nixos.org"
           "https://cache.garnix.io"
-          "https://vicinae.cachix.org"
+          # "https://vicinae.cachix.org"
         ];
         trusted-public-keys = [
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-          "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+          # "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
         ];
         connect-timeout = 5;
         stalled-download-timeout = 5;
