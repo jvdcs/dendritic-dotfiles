ee116fdef52279583c3f41815db0641dd43f3112
jvdcs <javedcorreossial@gmail.com>
2026-08-19 21:32:37 +0800

    fix

--- tree/definitions/users/alice/dotfiles/.config/yazi/init.lua
+++ tree/definitions/users/alice/dotfiles/.config/yazi/init.lua
@@ -7,3 +7,18 @@ hide_flags = false,
 flags_after_prompt = true,
 config_file = "~/.config/starship-yazi.toml",
 })
+
+-- FROM DOCS
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.mtime or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+end
